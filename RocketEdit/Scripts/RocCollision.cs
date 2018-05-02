d3a9c7469167e65978937cc733168ea6411ba50d
c16366366 <36455355+c16366366@users.noreply.github.com>
2018-05-02 13:32:47 +0100

    Add files via upload

---
+++ RocketEdit/Scripts/RocCollision.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine;
+
+public class RocCollision : MonoBehaviour {
+
+    public RocketMovement movement;  
+
+         void OnCollisionEnter(Collision collisionInfo)
+         {
+
+            if (collisionInfo.collider.tag == "Rings")
+            {
+
+				movement.enabled = false;
+
+            }
+
+         }
+    
+    
+
+}
